c62a7db01c631db7a0601128a86155cb0d163e58
xarturia <gianlvp19@gmail.com>
2021-08-10 22:32:38 -0300

    complementario

--- desafio_clase4/scripts/complementario.js
+++ desafio_clase4/scripts/complementario.js
@@ -1,5 +1,18 @@
-calcExcedente();
+/*
 
+Este script calcula el porcentaje de Impuestos a Bienes Personales que una persona debe pagar dependiendo del monto del mismo.
+
+Hice una función con otra dentro como para que quede prolijo el código.
+La primera parte de la función almacena el valor de los bienes que el usuario ingresa en la variable 'bienes', luego se define el cálculo de los porcentajes y el monto a pagar siguiendo la sig. fórmula:
+// (bienes / 100) * porcentaje //
+
+hice la constante 'mensaje' para no tener el código tan junto y que se diferencie del resto.
+Y por último, la función calcBienes mira el valor ingresado por el usuario, hace una comparación y dependiendo de si el monto es mayor o menor a determinada cifra, que muestre un alert.
+*/
+
+
+
+calcExcedente();
 function calcExcedente() {
     let bienes = prompt('Ingrese valor de sus bienes personales en pesos. Por ejemplo: ' +
         '\n1694767 (sin puntos y/o comas)');
@@ -26,16 +39,19 @@ function calcExcedente() {
             '\nEl impuesto aplicado es del ' + porcentaje[3] + '%' +
             '\nEl monto total a pagar calculado es: ' + excedente.mayor18M + ' pesos.'
     };
-
-    if (bienes <= 2000000) {
-        alert(mensaje.mensaje1);
-    } else if (bienes > 2000000 && bienes <= 3000000) {
-        alert(mensaje.mensaje2);
-    } else if (bienes > 3000000 && bienes <= 6500000) {
-        alert(mensaje.mensaje3);
-    } else if (bienes > 6500000 && bienes <= 18000000) {
-        alert(mensaje.mensaje4)
-    } else if (bienes >= 18000000) {
-        alert(mensaje.mensaje5)
+    function calcBienes(){
+        if (bienes <= 2000000) {
+            parseFloat(alert(mensaje.mensaje1));
+        } else if (bienes > 2000000 && bienes <= 3000000) {
+            parseFloat(alert(mensaje.mensaje2));
+        } else if (bienes > 3000000 && bienes <= 6500000) {
+            parseFloat(alert(mensaje.mensaje3));
+        } else if (bienes > 6500000 && bienes <= 18000000) {
+            parseFloat(alert(mensaje.mensaje4));
+        } else if (bienes >= 18000000) {
+            parseFloat(alert(mensaje.mensaje5));
+        }
     }
+    calcBienes();
+    calcExcedente();
 }
