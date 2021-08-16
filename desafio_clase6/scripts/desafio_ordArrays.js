9c41e28f3d2c18ec8658c4a865e8d997b84d73b7
xarturia <gianlvp19@gmail.com>
2021-08-16 01:36:26 -0300

    ordenar arrays

--- desafio_clase6/scripts/desafio_ordArrays.js
+++ desafio_clase6/scripts/desafio_ordArrays.js
@@ -19,7 +19,7 @@ const loggedUsers = ['Usuario1',
 console.log(loggedUsers.sort()); //Utilizo sort para ordenar los nombres alfabéticamente de A-Z, como no funciona de la misma manera en números, estos estarán posicionados antes que las letras.
 
 console.log('Números ordenados de mayor a menor');
-let numeroRaro = (98989898989899*98)/86 * Math.sqrt(8.6);
+let numeroRaro = (98989898989899 * 98) / 86 * Math.sqrt(8.6);
 const numeritos = [1, 109231, 2223, 54, 5 + 10, (7 * 99) / 3.33, 0, 0.9, 1.33 * 0.1, 23, 'a' - 5, numeroRaro]; // a - 5 es NaN.
 // ordena de mayor a menor los números dados
 console.log((numeritos.sort(function (a, b) {
@@ -32,3 +32,48 @@ console.log((numeritos.sort(function (a, b) {
     return a - b
 })));
 
+let genteRandom = [{
+    nombreComp: 'Santiago Moreno Charpentier',
+    edad: 39
+}, {
+    nombreComp: "Jeanne d'Arc",
+    edad: 19
+}, {
+    nombreComp: 'Mirtha Legrand',
+    edad: 146002435
+}, {
+    nombreComp: 'David Draiman (el pelado de Disturbed)',
+    edad: 48
+}]
+// Sin ordenar
+console.table(genteRandom);
+
+// Ordenando por menor a mayor edad
+console.table(genteRandom.sort((a, b) => a.edad - b.edad));
+
+// Ordenado de mayor a menor edad 
+console.table(genteRandom.sort((a, b) => b.edad - a.edad));
+
+// Ordenado alfabéticamente A-Z
+aZGenteRandom = genteRandom.sort((a, b) => {
+    if (a.nombreComp > b.nombreComp) {
+        return 1;
+    }
+    if (a.nombreComp < b.nombreComp) {
+        return -1;
+    }
+    return 0;
+});
+console.table(aZGenteRandom);
+
+// Ordenado Alfabéticamente Z-A
+zAGenteRandom = genteRandom.sort((a, b) => {
+    if (a.nombreComp > b.nombreComp) {
+        return -1;
+    }
+    if (a.nombreComp < b.nombreComp) {
+        return 1;
+    }
+    return 0;
+});
+console.table(zAGenteRandom);
