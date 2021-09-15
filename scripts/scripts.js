decc17f3ae0a43d427cd3be2d7b20a80cd84b40d
xarturia <gianlvp19@gmail.com>
2021-09-14 23:06:09 -0300

    Utilizando  pokeapi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,8 +29,16 @@ $(function () {
                 let status = capitalize(response); //estado de la petición
                 let image = pokemon.sprites.other["official-artwork"].front_default; //imagen del pokemon
                 let url = $('#pokeurl');
-                let expBase = pokemon.base_experience;
+                let expBase = pokemon.base_experience; //experiencia
                 let ability = pokemon.abilities; //arrays con las habilidades
+                let weight = pokemon.weight;
+                let stats = pokemon.stats
+                let hp = stats[0].base_stat
+                let att = stats[1].base_stat
+                let def = stats[2].base_stat
+                let spAtt = stats[3].base_stat
+                let spDef = stats[4].base_stat
+                let speed = stats[5].base_stat
 
                 // HTTP REQUEST
                 if (response === 'success') { // en caso de success (200OK)
@@ -42,7 +50,7 @@ $(function () {
                     };;
                     showAbilities(); // muestra el array de objetos con las habilidades
 
-
+                    console.log(pokemon)
                     let abilityName = "";
                     // loopeo en 'ability' para poder extraer sólo los nombres
                     for (i = 0; i < ability.length; i++) {
@@ -59,14 +67,32 @@ $(function () {
                     <div class="card-body">
                       <h5 class="card-title">Pokestats</h5>
                       <h6 class="card-subtitle mb-2 text-muted"></h6>
-                      <p class="card-text">Estadísticas de ${pokeName} [${input.val()}]</p>
+                      <p class="card-text">Estadísticas de <span class="extraName">${pokeName}</span> [#${input.val()}]</p>
                       <ul>
-                        <li class="base_exp">Exp. Base: <span class="extraspan">${expBase}</span></li>
-                        <li class="ability">Habilidad: ${abilityName}<span class="extraspan"></span></li>
-                        <li class="moves"><span class="extraspan"></span></li>
-                    </ul>
-                      <a href="#" class="card-link">Pokedex</a>
-                      <a href="#" class="card-link"></a>
+                        <li class="base_exp moreInfo">Exp. Base: <span class="extraspan">${expBase}</span></li>
+                        <li class="ability moreInfo">Habilidad: <span class="extraspan">${abilityName}</span></li>
+                        <li class="weight moreInfo">Peso: <span class="extraspan">${parseFloat(weight/10)}kg</span></li>
+                       </ul>
+                      <div class="accordion" id="accordionExample">
+                      <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Base Stats
+                          </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                          <div class="accordion-body">
+                            <ul>
+                              <li class="health moreInfo">Base HP: <span class="extraspan">${hp}</span></li>
+                              <li class="att moreInfo">Base Atk.: <span class="extraspan">${att}</span></li>
+                              <li class="def moreInfo">Base Def.: <span class="extraspan">${def}</span></li>
+                              <li class="spatt moreInfo">Base Sp. Atk: <span class="extraspan">${spAtt}</span></li>
+                              <li class="spdef moreInfo">Base Sp. Def: <span class="extraspan">${spDef}</span></li>
+                              <li class="speed moreInfo">Base Speed: <span class="extraspan">${speed}</span></li>
+                          </ul>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>`)
                     //detalles del pokemon en la consola
